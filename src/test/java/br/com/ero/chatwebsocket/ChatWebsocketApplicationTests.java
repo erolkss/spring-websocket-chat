8dee86ac757729e56fc9dbcc8c062d98fa2f8366
Lucas Lima <lucaserolima@gmail.com>
2024-04-27 19:47:32 -0400

    First Commit - Create a Project Chat with Websocket

---
+++ src/test/java/br/com/ero/chatwebsocket/ChatWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.chatwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
