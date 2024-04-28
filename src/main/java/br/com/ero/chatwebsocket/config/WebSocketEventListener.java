3958b96f740afdadc6b8d3109452b9c72adbece4
Lucas Lima <lucaserolima@gmail.com>
2024-04-27 20:10:30 -0400

    feat: Add disconnect event listener.

---
+++ src/main/java/br/com/ero/chatwebsocket/config/WebSocketEventListener.java
@@ -0,0 +1,33 @@
+package br.com.ero.chatwebsocket.config;
+
+import br.com.ero.chatwebsocket.chat.ChatMessage;
+import br.com.ero.chatwebsocket.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageTemplates;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent disconnectEvent) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(disconnectEvent.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplates.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
